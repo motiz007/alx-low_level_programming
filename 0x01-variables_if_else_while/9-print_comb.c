73901888a0870d9052b3a79681e5f47d6f341e4e
motiz007 <francismototo@gmail.com>
2023-02-17 15:03:20 -0800

    Patience, persistence and perspiration

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,8 +10,11 @@ int main(void)
 	for (num = 0; num <= 9; num++)
 	{
 		putchar('0' + num);
-		putchar(',');
-		putchar(' ');
+	       if (num < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 	return (0);
