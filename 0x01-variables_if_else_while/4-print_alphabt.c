457f35967d14c44fe582a2f1431b34d287d0817c
motiz007 <francismototo@gmail.com>
2023-02-17 13:31:51 -0800

    alphabet skipping

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,8 +8,12 @@ int main(void)
 	char alpha;
 
 	for (alpha = 'a'; alpha < 'z'; alpha++)
+	{
 		if (alpha != 'e' && alpha != 'q')
+		{
 			putchar(alpha);
 			putchar('\n');
+		}
+	}
 	return (0);
 }
