a58c3e599b421bcb7bff20ddcbfc47e6cf221c7a
motiz007 <francismototo@gmail.com>
2023-02-17 13:27:31 -0800

    alphabet skipping

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main- prints lower alphabet except q an e
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha < 'z'; alpha++)
+		if (alpha != 'e' && alpha != 'q')
+			putchar(alpha);
+			putchar('\n');
+	return (0);
+}
