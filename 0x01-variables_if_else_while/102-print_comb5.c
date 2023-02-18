c401ee39a4e4fa71f7c79f164b5b0508a65129b3
motiz007 <francismototo@gmail.com>
2023-02-18 14:56:20 -0800

    software is eating the world

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,38 +1,27 @@
 #include <stdio.h>
 /**
- * main- prints two double digit nums
- * Return: 0 (success)
+ * main -Entry point
+ *
+ * Return:Always 0 (Success)
  */
 int main(void)
 {
 	int num1;
 	int num2;
-	int num3;
-	int num4;
 
-	for (num1 = 0; num1 <= 9; num1++)
+	for (num1 = 0; num1 <= 98; num1++)
 	{
-		for (num2 = 0; num2 <= 9; num2++)
+		for (num2 = num1 + 1; num2 <= 99; num2++)
 		{
-			for (num3 = 0; num3 <= 9; num3++)
-			{
-				for (num4 = 0 + 1; num4 <= 9; num4++)
-				{
-					if (((num3 + num4) > (num1 + num2) && num3 >= num1) || num1 < num3)
-					{
-						putchar('0' + num1);
-						putchar('0' + num2);
-						putchar(' ');
-						putchar('0' + num3);
-						putchar('0' + num4);
-						if (!(num1 == 9 && num2 == 8 && num3 == 9 && num4 == 9))
-						{
-							putchar(',');
-							putchar(' ');
-						}
-					}
-				}
-			}
+			putchar((num1 / 10) + '0');
+			putchar((num1 % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num1 == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
