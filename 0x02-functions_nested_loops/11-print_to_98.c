afe05a3d3bec1860152a84c97e45361c98de8a55
motiz007 <francismototo@gmail.com>
2023-02-21 16:09:28 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -7,29 +7,26 @@
  */
 void print_to_98(int n)
 {
-	int diff;
-	int i;
+	int i, j;
 
-	diff = n - 98;
-	if (diff > 0)
+	if (n <= 98)
 	{
-		for (i = n; i >= 98; i--)
+		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i != 98)
+				printf("%d, ", i);
+			else if (i == 98)
+				printf("%d\n", i);
 		}
-		putchar('\n');
 	}
-	else if (diff < 0)
+	else if (n >= 98)
 	{
-		for (i = n; i >= 98; i++)
+		for (j = n; j >= 98; j--)
 		{
-			printf("%d, ", i);
+			if (j != 98)
+				printf("%d, ", j);
+			else if (j == 98)
+				printf("%d\n", j);
 		}
-		putchar('\n');
 	}
-	else
-	{
-		printf("%d\n", n);
-	}
-}
-
+}	
