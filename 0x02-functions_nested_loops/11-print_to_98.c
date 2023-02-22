61823d913492e6e8d649df5d87354530d7651fdc
motiz007 <francismototo@gmail.com>
2023-02-21 16:00:00 -0800

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- prints from n to 98
+ * @n: the number it recieves and counts from
+ * Return: the numbers to 98
+ */
+void print_to_98(int n)
+{
+	int diff;
+
+	diff = n - 98;
+	absdiff = diff * -1;
+	if (diff > 0)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d, ", i);
+		}
+		putchar('\n');
+	}
+	else if (diff < 0)
+	{
+		for (i = n; i >= 98; i++)
+		{
+			printf("%d, ", i);
+		}
+		putchar('\n');
+	}
+	else
+	{
+		printf("%d\n", n);
+	}
+}
+
