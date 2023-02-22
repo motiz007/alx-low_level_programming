c9ef25b4f0251731a1e9e1f5fb8fffe52b78b552
motiz007 <francismototo@gmail.com>
2023-02-22 13:57:36 -0800

    nature and natural numbers

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,36 +5,4 @@
  * Return: void
  */
 void print_times_table(int n)
-{
-	if (n <= 15 && n >= 0)
-	{
-		int i, j, product, divisor;
 
-		for (i = 0; i <= n; i++)
-		{
-			for (j = 0; j <= n; j++)
-			{
-				product = i * j;
-				if (product < 0)
-				{
-					_putchar('-');
-					product = -product;
-				}
-				divisor = 1;
-				while (divisor <= product / 10)
-				{
-					divisor *= 10;
-				}
-				while (divisor > 0)
-				{
-					_putchar('0' + (product / divisor));
-					product %= divisor;
-					divisor /= 10;
-				}
-				_putchar(',');
-				_putchar(' ');
-			}
-			_putchar('\n');
-		}
-	}
-}
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main- computes and prints the sum of all the multiples of 3 or 5 below 1024
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int i;
+	int sum;
+
+	for (i = 0; i < 1024; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			i += i;
+			printf("%d\n", i);
+		}
+	}
+	return (0);
+}
