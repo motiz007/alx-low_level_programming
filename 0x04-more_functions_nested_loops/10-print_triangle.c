b9cd0b1e853cac33f35c7a6b3d26795768992fc5
motiz007 <francismototo@gmail.com>
2023-02-23 14:45:15 -0800

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,25 +6,25 @@
  */
 void print_triangle(int size)
 {
-	int i;
-	int j;
-
 	if (size <= 0)
 	{
 		_putchar('\n');
-	}
-	else
+	} else
 	{
-		for (i = 0; i < size; i++)
+		int i, j;
+
+		for (i = 1; i <= size; i++)
 		{
 			for (j = i; j < size; j++)
 			{
 				_putchar(' ');
 			}
-			for (j = 0; j <= i; j++)
+
+			for (j = 1; j <= i; j++)
 			{
 				_putchar('#');
 			}
+
 			_putchar('\n');
 		}
 	}
