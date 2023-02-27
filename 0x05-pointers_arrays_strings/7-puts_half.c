520e469e80f5d925118a217a5d3773293ded5f48
motiz007 <francismototo@gmail.com>
2023-02-28 00:38:02 +0300

    half string

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -7,26 +7,13 @@
 void puts_half(char *str)
 {
 	int i;
-	int half, len;
 
 	for (i = 0; str[i] != '\0'; i++)
+		;
+	i++;
+	for (i /= 2; str[i] != '\0'; i++)
 	{
-		len++;
-	}
-	if (len % 2 == 0)
-	{
-		half = len / 2;
-	}
-	else
-	{
-		half = (len - 1) / 2;
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i >= half && i <= len)
-		{
-			_putchar(str[i]);
-		}
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
