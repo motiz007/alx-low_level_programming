d4180a8e0251537ea94bf5631673ab1ec35b1df7
motiz007 <francismototo@gmail.com>
2023-03-01 22:53:29 +0300

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,9 +13,9 @@ char *_strncpy(char *dest, char *src, int n)
 
 	while (src[i++])
 	{
-	len++;
+		len++;
 	}
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n && src[i]; i++)
 	{
 		dest[i] = src[i];
 	}
