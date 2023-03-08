d85b9665ffa0418588491887d2a5fecb192fa0b7
motiz007 <francismototo@gmail.com>
2023-03-08 11:54:49 -0800

    _strlen recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen_recursion- prints string length
+ * @s: the string it recieves
+ * Return: length
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s)
+	{
+		return (1 + (_strlen_recursion(s + 1)));
+	}
+	else
+	return (0);
+}
