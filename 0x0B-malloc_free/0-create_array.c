4ea610ead408b5544ed209accdb339fe83f5b4e9
motiz007 <francismototo@gmail.com>
2023-03-13 12:45:20 -0700

    array malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,7 +12,11 @@ char *create_array(unsigned int size, char c)
 	char *ptr;
 	unsigned int i;
 
+	if (size == 0)
+		return (NULL);
 	ptr = malloc(size * (sizeof(char)));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		ptr[i] = c;
