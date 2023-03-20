b3d59771c33f5506610a9238ea8804ab2faa24e5
motiz007 <francismototo@gmail.com>
2023-03-20 10:10:53 -0700

    program that prints the name of the file using a macro

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 /**
- * main- prints the name of the file it was compiled from, followed by a new line.
+ * main- prints name of the file, followed by a new line.
  * Return: 0 (success)
  */
-#define FILENAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
+#define NAME (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 int main(void)
 {
-	printf(FILENAME);
+	printf(NAME);
 	printf("\n");
 	return (0);
 }
