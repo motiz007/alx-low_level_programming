d6de41fb99f382e985abb959669ecca8e69a3fe7
motiz007 <francismototo@gmail.com>
2023-04-04 14:41:00 -0700

    addition of function that deletes nth node

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+/**
+ * delete_nodeint_at_index - deletes the node at index
+ * 		 	     index of a listint_t linked list
+ * @head: the linked list
+ * @index: the index to delete
+ * Return: 1, -1 if failed
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *tmp, *cpy = *head;
+	unsigned int node;
+
+	if (cpy == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(cpy);
+		return (1);
+	}
+
+	for (node = 0; node < (index - 1); node++)
+	{
+		if (cpy->next == NULL)
+			return (-1);
+
+		cpy = cpy->next;
+	}
+
+	tmp = cpy->next;
+	cpy->next = tmp->next;
+	free(tmp);
+	return (1);
+}
