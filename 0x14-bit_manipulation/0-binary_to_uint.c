c1d12fd5f6975b86d02d915ca923b9beafd1f4ab
motiz007 <francismototo@gmail.com>
2023-04-07 14:31:28 -0700

    addition of binary to decimal converter

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unsigned int
+ * @b: the binary number
+ * Return: the converted number
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0;
+
+	if (b == NULL)
+		return (0);
+	while (*b != '\0')
+	{
+		if (*b < '0' || *b > '1')
+			return (0);
+		if (*b == '1')
+			num = num * 2 + 1;
+		if (*b == '0')
+			num = num * 2 + 0;
+		b++;
+	}
+	return (num);
+}
