5bb3e5b2b61cb75aeeb74dca8d00bf4d30635703
motiz007 <francismototo@gmail.com>
2023-04-08 05:38:32 -0700

    addition of a get_bit function

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * get_bit - gets value of binary at an index
+ * @n: the number to convert
+ * @index: the position to get
+ * Return: the value, -1 if error
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int base = 1;
+	int ans;
+
+	if (index > 32)
+		return (-1);
+	base = base << index;
+	ans = n & base;
+	if (ans > 0)
+		return (1);
+	return (0);
+}
