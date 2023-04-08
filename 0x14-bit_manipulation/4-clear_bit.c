50ee2a4f8958d45703a94099774ffdf06e9321e8
motiz007 <francismototo@gmail.com>
2023-04-08 09:21:57 -0700

    addition of clear_bit function

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * clear_bit - sets nth bit to 0
+ * @n: the number to convert
+ * @index: the position to switch
+ * Return: 1 success, -1 error
+ */
+
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+	int base = 1;
+	int num;
+
+	num = *n;
+	if (index > 32)
+		return (-1);
+	base = base << index;
+	base = ~base;
+	num = num & base;
+	*n = num;
+	return (1);
+}
