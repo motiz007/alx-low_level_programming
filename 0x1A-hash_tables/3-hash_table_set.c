80c85919267ac297d6cf6345faec2185288a865b
motiz007 <francismototo@gmail.com>
2023-07-08 02:19:04 +0300

    function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht->array[index] == NULL)
 		ht->array[index] = new_node;
 	else
-		collision_handler(ht->array[index], new_node);
+		collision_handler(&(ht->array[index]), new_node);
 
 	return (1);
 }
