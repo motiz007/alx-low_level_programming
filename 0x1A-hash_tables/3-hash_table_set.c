dca9a2d2de68266b07372828c3615a643224bb00
motiz007 <francismototo@gmail.com>
2023-07-08 02:20:33 +0300

    function that adds an element to the hash table.

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -41,7 +41,7 @@ hash_node_t *create_node(const char *key, const char *value)
 	{
 		return (NULL);
 	}
-	
+
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
