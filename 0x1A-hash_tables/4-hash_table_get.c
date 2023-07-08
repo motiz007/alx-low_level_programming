ae6d2887d48b365a8411c7e70bddc8cc9da20b42
motiz007 <francismototo@gmail.com>
2023-07-08 03:24:24 +0300

    function that retrieves a value associated with a key.

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -20,7 +20,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	{
 		if (strcmp(key, temp->key) == 0)
 		{
-			info = strdup(temp->value);
+			info = temp->value;
 			return (info);
 		}
 		temp = temp->next;
