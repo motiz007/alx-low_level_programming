223f4a7915991b59da629c7e9ded9ed59f21f0c7
motiz007 <francismototo@gmail.com>
2023-07-08 04:18:41 +0300

    function that prints a hash table

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -21,7 +21,7 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (comma == 1)
 				printf(", ");
-			printf("'%s' : '%s'", temp->key, temp->value);
+			printf("'%s': '%s'", temp->key, temp->value);
 			comma = 1;
 			temp = temp->next;
 		}
